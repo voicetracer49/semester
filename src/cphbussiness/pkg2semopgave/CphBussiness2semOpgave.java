529ccedbb9e20293a81e45b90d17ab361796467a
Ejer <Ejer@10.18.138.3>
2014-03-06 13:30:47 +0100

    objbc7.jar driver implementeret i projectet
    CphBussiness-2semOpgave

---
+++ src/cphbussiness/pkg2semopgave/CphBussiness2semOpgave.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cphbussiness.pkg2semopgave;
+
+/**
+ *
+ * @author https://github.com/voicetracer49/CphBussiness-2semOpgave.git Bettina,Anders, Marcus, Tanja, Ruben
+ * Before you Start http://www.git-tower.com/learn/version-control-best-practices.html
+ */
+public class CphBussiness2semOpgave
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+}

f455117603361f19beae2498b8e368f06b5710ed
Anders Kjær <Anders Kjr@10.18.137.173>
2014-03-24 10:40:48 +0100

    Adding the first simple GUI

--- src/DataSource/DBConnector.java
+++ src/DataSource/DBConnector.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package DataSource;
 
 import java.sql.Connection;
@@ -15,34 +14,29 @@ import java.sql.DriverManager;
  */
 public class DBConnector {
 
-	  private static String id = "cphak160";				
-	  private static String pw = "cphak160";
-	 
-	  public Connection getConnection()
-	  {
-	    Connection con = null;
-	    try 
-	    {  
-	      Class.forName("oracle.jdbc.driver.OracleDriver");
-	      con = DriverManager.getConnection(
-	          "jdbc:oracle:thin:@datdb.cphbusiness.dk:1521:dat", id,  pw );  
-	    }
-	    catch (Exception e) 
-	    {   
-	    	System.out.println("\n*** Remember to insert your Oracle ID and PW in the DBConnector class! ***\n");
-	    	System.out.println("eror in DBConnector.getConnection()");
-	        System.out.println(e); 	     
-	    }    
+    private static String id = "SEM2_TEST_GR05";
+    private static String pw = "SEM2_TEST_GR05";
 
-	    return con;
-	  }
-	  
-	  public void releaseConnection(Connection con)
-	  {
-	      try{
-	          con.close();
-	      }
-	      catch (Exception e)
-	      { System.err.println(e);}
-	  }
+    public Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            con = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@datdb.cphbusiness.dk:1521:dat", id, pw);
+        } catch (Exception e) {
+            System.out.println("\n*** Remember to insert your Oracle ID and PW in the DBConnector class! ***\n");
+            System.out.println("eror in DBConnector.getConnection()");
+            System.out.println(e);
+        }
+
+        return con;
+    }
+
+    public void releaseConnection(Connection con) {
+        try {
+            con.close();
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
 }
